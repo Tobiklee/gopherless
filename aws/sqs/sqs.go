00c6722bc2893c2305fc8af46e2ba71bfc5e5ca9
Tobias Kleemann <tobias.kleemann@quirion.de>
2021-10-27 17:34:34 +0200

    Added New to sqs service

--- aws/sqs/sqs.go
+++ aws/sqs/sqs.go
@@ -4,12 +4,31 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
 type Service struct {
 	Client *sqs.Client
 	URL    string
+}
+
+// New creates a new service instance
+func New(region, url string) *Service {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), func(opts *config.LoadOptions) error {
+		opts.Region = region
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	client := sqs.NewFromConfig(cfg)
+
+	return &Service{
+		Client: client,
+		URL:    url,
+	}
 }
 
 // SendMessage sends a message to the initialized URL
